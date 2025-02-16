265466a55d46d45803ccae89054023a46fd8ae80
Anton Wittig <anton.wittig@t-online.de>
2025-02-16 19:40:23 +0100

    Add debugging details to site generation script

--- static-site-gen.sh
+++ static-site-gen.sh
@@ -19,11 +19,13 @@ do
 done
 
 # Copy files from source to destination directory
+echo "Start searching for files with regex: $fileRegex"
 for file in $(find "${sourceDir}" -regex "${fileRegex}");
 do
 	if [[ "${filesToExclude[*]}" =~ "${file}" ]];
 	then
-		break
+		echo "Excluding the file: $file"
+		continue
 	fi
 
 	relativePath=$(realpath --relative-to="${sourceDir}" "${file}" | sed "s/.php$/.html/")
@@ -32,12 +34,14 @@ do
 
 	if [[ "${file}" == *".php" ]];
 	then
+		echo "Convert $file to $newFile"
 		# Execute php on php files
 		php -c . -f "${file}" > "${newFile}.tmp"
 		sed -i "s/.php/.html/gm" "${newFile}.tmp"
 		iconv -t UTF-8 "${newFile}.tmp" > "${newFile}"
 		rm "${newFile}.tmp"
 	else
+		echo "Copy $file to $newFile"
 		iconv -t UTF-8 "${file}" > "${newFile}"
 	fi
 done
