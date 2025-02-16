f0bd84e5ba2735d02ffc75e5587d61451657dc05
Anton Wittig <anton.wittig@t-online.de>
2025-02-16 17:30:53 +0100

    feat: basic contact form

--- public/contact.php
+++ public/contact.php
@@ -3,7 +3,12 @@ $title = "Anton Wittig";
 $current_page = "contact.php";
 
 ob_start(); ?>
-Here a form for sending a contact request will appear.
+<form method="POST" action="http://212.132.110.174:8273">
+			<input type="email" name="email" />
+			<input type="text" name="subject" />
+			<input type="text" name="message" />
+			<input type="submit">
+		</form>
 <?php $content = ob_get_clean();
 
 include "template.php";
