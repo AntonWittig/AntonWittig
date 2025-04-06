20afb445cb4dcbc42361007a9ba29792534034a2
Anton Wittig <anton.wittig@t-online.de>
2025-04-06 19:50:07 +0200

    Update index content

--- public/index.php
+++ public/index.php
@@ -3,7 +3,17 @@ $title = "Anton Wittig";
 $current_page = "index.php";
 
 ob_start(); ?>
-Hallo und herzlich willkommen auf <strong>dieser</strong> Website!
+<p>
+	<strong>Willkommen</strong><br />
+	Mein Name ist Anton Wittig, ich bin leidenschaftlicher Softwareentwickler.
+</p>
+<p>
+	Auf dieser Website biete ich einen Einblick in meinen beruflichen Werdegang, meine bisherigen Projekte, meine
+	persönlichen Interessen, und die Möglichkeit mit mir in Kontakt zu treten.
+</p>
+<p>
+	Ich freue mich über Ihr Interesse!
+</p>
 <?php $content = ob_get_clean();
 
 include "template.php";
